79785a87df859f315ece456d43b391e74502b7d1
Srikeerti <srikeerti@gmail.com>
2019-07-24 17:03:31 +0530

    Ovia JSON to CSV

--- json_flatten.py
+++ json_flatten.py
@@ -1,28 +1,21 @@
-def flattenjson( b, delim ):
-    val = {}
-    for i in b.keys():
-        if isinstance( b[i], dict ):
-            get = flattenjson( b[i], delim )
-            for j in get.keys():
-                val[ i + delim + j ] = get[j]
-        else:
-            val[i] = b[i]
-
-    return val
-
-flattenjson( {
-    "pk": 22, 
-    "model": "auth.permission", 
-    "fields": {
-      "codename": "add_message", 
-      "name": "Can add message", 
-      "content_type": 8
-    }
-  }, "__" )
-  
-out_dict = flattenjson(input,"__")
-
-with open('mycsvfile.csv', 'w') as f:  # Just use 'w' mode in 3.x
-    w = csv.DictWriter(f, out_dict.keys())
-    w.writeheader()
-    w.writerow(out_dict)
+with open('sample.json','r') as f:
+    data = json.load(f)
+ovia = data['report']['records']
+count = 0
+for i in ovia:
+    if count ==0:
+        print("Going to write CSV Header")
+        #print(type(i))
+        #print(list(i.keys()))
+        header = list(i.keys())
+        with open('file.csv', 'w', newline='') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            wr.writerow(header)
+        count+=1
+    #else:
+    print("Going to write CSV data")
+    #print(list(i.values()))
+    values = list(i.values())
+    with open('file.csv', 'a', newline='') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            wr.writerow(values)
